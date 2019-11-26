abb69a40c2c99912491a51381e660ac5357d8b35
Reid Hanson <reidhans@buffalo.edu>
2019-11-26 16:20:44 -0500

    homepage update

---
+++ src/components/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { link } from "react-router-dom";
+
+const HomePage = () => {
+const [page, setPage] = useState= (1)
+const topShowAPI = async () =>
+await axios.get (
+  `https://api.themoviedb.org/3/tv/top_rated?api_key=9c1bbfefb575f9cb6c182e019bc74b99&language=en-US&page=${page}`
+  )
+  
+}
+}
+
+export default HomePage;
