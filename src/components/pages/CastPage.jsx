f7d6b0cc05e7d5192a188b9245e782c52b9c8e9f
Celeste Sippel <celestechristine@gmail.com>
2019-11-26 18:00:56 -0500

    added castPage

--- src/components/pages/CastPage.jsx
+++ src/components/pages/CastPage.jsx
@@ -14,10 +14,39 @@ const CastPage = props => {
       `https://api.themovieb.org/3/tv${showData.id}/credits?api_key=9c1bbfefb575f9cb6c182e019bc74b99&language=en-US`
     )
     setCastData(resp.data.cast)
+    setCrewData(resp.data.crew.slice(0, 10))
   }
+  console.log(props.match.params)
+  useEffect(() => {
+    getApiData()
+  }, [])
+  return (
+    <>
+      <section className="cast-page">
+        <section className="show-details"></section>
+        <img scr="" alt="" />
+        <h2>{showData.name}</h2>
+        <p>{showData.overview}</p>
+      </section>
+      <section className="crew-details">
+        <h2 className="crewName">Crew</h2>
+        <section className="crew-info">
+          {crewData.map((info, j) => {
+            return <Crew key={j} info={info} />
+          })}
+        </section>
+      </section>
 
-const CastPage = () => {
-  return <div></div>
+      <section className="cast-details">
+        <h2>Cast</h2>
+        <section className="cast-info">
+          {castData.map((item, i) => {
+            return <Cast key={i} item={item} />
+          })}
+        </section>
+      </section>
+    </>
+  )
 }
 
 export default CastPage
