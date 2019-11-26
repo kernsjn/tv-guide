a63e74e77a29d951f0d66302c03f1ccdd47f1338
Celeste Sippel <celestechristine@gmail.com>
2019-11-26 15:07:30 -0500

    added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <>
+      <main>
+        <section>
+          <img src="" />
+          <header></header>
+        </section>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
+      </main>
+    </>
+  )
+}
+
+export default Header
